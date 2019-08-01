78bb7773e87665cd2f6ed7fb23b0642c1af114ab
kiba518 <liujin_hzzh@126.com>
2019-08-01 09:39:08 +0800

    WPF滑块控件(Slider)的自定义样式
    WPF滑块控件(Slider)的自定义样式

---
+++ WpfSlider/UCSlider3.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSlider
+{
+     
+    public partial class UCSlider3 : UserControl
+    {
+        public UCSlider3()
+        {
+            InitializeComponent();
+        }
+    }
+}
